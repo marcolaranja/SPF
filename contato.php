0f6f541c45f89af67af56994c17d2b1b9aa42d39
Marco Laranja <marco_laranja@hotmail.com>
2017-11-16 10:36:06 -0200

    formulario incompleto

--- contato.php
+++ contato.php
@@ -4,6 +4,12 @@
 
          <h1>Contato</h1>
 
+         <form action="#" method="post" name="contato">
+           <p>Name:<br></p>
+           <input type="text" name="username"><br>
+           <input type="submit" value="Submit">
+         </form>
+
    </div>
 
   <?php include('./footer.php'); ?>
