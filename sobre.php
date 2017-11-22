a64594c9329ba04326efad8e4fcc9e6a0ce221ac
Marco Laranja <marco_laranja@hotmail.com>
2017-11-22 13:53:59 -0200

    sobre

--- sobre.php
+++ sobre.php
@@ -1,19 +1,19 @@
-  <?php include('./header.php'); ?>
+<?php include('./header.php'); ?>
 
-   <div class="container">
+<div class="container">
 
-         <article class="text-center">
+  <article class="text-center">
 
-           <h1>Sobre</h1>
+    <h1>Sobre</h1>
 
-         <p>Super Professional Fighters é um jogo de luta no qual você poderá selecionar entre vários profissionais
-         para lutar contra outros profissionais e assim representar sua área e adquirir o subsídio do presidente para
-     	 mostrar que você é um mestre na sua área de atuação.</p>
+      <p>Super Professional Fighters é um jogo de luta no qual você poderá selecionar entre vários profissionais
+      para lutar contra outros profissionais e assim representar sua área e adquirir o subsídio do presidente para
+      mostrar que você é um mestre na sua área de atuação.</p>
 
-     	 <p class="text-center">Você podera escolher entre vários profissionais, desde um mestre cervejeiro a até um soldado.</p>
+      <p>Você podera escolher entre vários profissionais, desde um mestre cervejeiro a até um soldado.</p>
 
-          </article>
+    </article>
 
-   </div>
+  </div>
 
   <?php include('./footer.php'); ?>
